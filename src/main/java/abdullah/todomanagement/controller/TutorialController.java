57676dcebee96c42a985ed3a1146f0ece9b322b6
abdullah <syedabdullahrahman@gmail.com>
2022-03-13 00:18:17 +0600

    cors

--- src/main/java/abdullah/todomanagement/controller/TutorialController.java
+++ src/main/java/abdullah/todomanagement/controller/TutorialController.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200","http://todo-sync-token.herokuapp.com"})
 public class TutorialController {
 
 	@Autowired
